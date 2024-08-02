26e79520d50e2eb13c0878115ad6118bc58b974e
Jeremy <jpwijanto@gmail.com>
2024-08-02 14:18:39 +1000

    adjust layout

--- src/components/EquipmentPopup.tsx
+++ src/components/EquipmentPopup.tsx
@@ -8,10 +8,15 @@ interface EquipmentPopupProps {
 const EquipmentPopup: React.FC<EquipmentPopupProps> = ({type, onClose}) =>{
   return (
     <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center' onClick={onClose}>
-    <div className='bg-white p-4 rounded'>
-      <h2 className='text-2xl text-red-500'> {type}</h2>
-      {/* Add your equipment selection logic here */}
-      <button onClick={onClose} className='mt-4 p-2 bg-red-500 text-white rounded'>Close</button>
+    <div className='bg-white p-4 rounded w-[600px]'>
+      <div className="flex justify-between">
+        <h2 className='text-2xl text-red-500'> {type}</h2>
+        <button onClick={onClose} className='p-2 bg-red-500 text-white rounded'>Close</button>
+      </div>
+      <div>
+        {/* this is where i map the equipment to display it base on type selected*/}
+        
+      </div>
     </div>
   </div>
   )
